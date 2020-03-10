f405c10e057b3d995fabf67a464e7f1cc9a2462c
Bryn <53411098+COFFEEEADDICT@users.noreply.github.com>
2020-03-10 15:08:44 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-  const textToChange = document.querySelector("h1")
+   const textToChange = document.querySelector("h1")
 
-document.addEventListener('DOMContentLoaded', init, false);
-function init() {
-	console.log('Engage');
-	window.addEventListener('devicemotion', motion, false);
-}
+// document.addEventListener('DOMContentLoaded', init, false);
+// function init() {
+// 	console.log('Engage');
+// 	window.addEventListener('devicemotion', motion, true);
+// }
 
-let lastX, lastY, lastZ;
-let moveCounter = 0;
+// let lastX, lastY, lastZ;
+// let moveCounter = 0;
 
-function motion(e) {
-	let acc = e.acceleration;
-	if(!acc.hasOwnProperty('x')) {
-		acc = e.accelerationIncludingGravity;
-	}
+// function motion(e) {
+// 	let acc = e.acceleration;
+// 	if(!acc.hasOwnProperty('x')) {
+// 		acc = e.accelerationIncludingGravity;
+// 	}
 
-	if(!acc.x) return;
+// 	if(!acc.x) return;
 
-	//only log if x,y,z > 1
-	if(Math.abs(acc.x) >= 1 &&
-	Math.abs(acc.y) >= 1 &&
-	Math.abs(acc.z) >=1) {
-		//console.log('motion', acc);
-		if(!lastX) {
-			lastX = acc.x;
-			lastY = acc.y;
-			lastZ = acc.z;
-			return;
-		}
+// 	//only log if x,y,z > 1
+// 	if(Math.abs(acc.x) >= 1 &&
+// 	Math.abs(acc.y) >= 1 &&
+// 	Math.abs(acc.z) >=1) {
+// 		//console.log('motion', acc);
+// 		if(!lastX) {
+// 			lastX = acc.x;
+// 			lastY = acc.y;
+// 			lastZ = acc.z;
+// 			return;
+// 		}
 
-		let deltaX = Math.abs(acc.x - lastX);
-		let deltaY = Math.abs(acc.y - lastY);
-		let deltaZ = Math.abs(acc.z - lastZ);
+// 		let deltaX = Math.abs(acc.x - lastX);
+// 		let deltaY = Math.abs(acc.y - lastY);
+// 		let deltaZ = Math.abs(acc.z - lastZ);
 		
-		if(deltaX + deltaY + deltaZ > 3) {
-			moveCounter++;
-		} else {
-			moveCounter = Math.max(0, --moveCounter);
-		}
+// 		if(deltaX + deltaY + deltaZ > 3) {
+// 			moveCounter++;
+// 		} else {
+// 			moveCounter = Math.max(0, --moveCounter);
+// 		}
 
-		if(moveCounter > 2) {
-			textToChange.innerText = "SHAKED";
-			moveCounter = 0;
-		}
+// 		if(moveCounter > 2) {
+// 			textToChange.innerText = "SHAKED";
+// 			moveCounter = 0;
+// 		}
 
-		lastX = acc.x;
-		lastY = acc.y;
-		lastZ = acc.z;
+// 		lastX = acc.x;
+// 		lastY = acc.y;
+// 		lastZ = acc.z;
 		
-	}
-}
+// 	}
+// }
 
-// window.addEventListener('devicemotion', function(event) {
-//    textToChange.innerText = "SHAKED";
-// });
+window.addEventListener('devicemotion', function(event) {
+   textToChange.innerText = "SHAKED";
+});
